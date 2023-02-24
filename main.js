4c7a2f6a66a965ce4e949a3216c592094aa59b50
The Doctor <learicist@gmail.com>
2023-02-24 00:51:57 -0500

    begin navbar styling, add JavaScript file, acquire color scheme from reference page. 2 hours

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    // MOUSEENTER NAV EFFECTS
+    $('#navMain').on({
+        mouseenter: function () {
+            if (window.innerWidth >= 581) {
+                $(this).css("background-color", "rgba(0, 115, 230, 0.40)");
+                //$(".links").css("color", "white");
+            }             
+        },
+        mouseleave: function () {
+            if (window.innerWidth >= 581) {
+                $(this).css("background-color", "rgb(255, 255, 255, 0.25)");
+            }
+        }
+    });
+
+});
